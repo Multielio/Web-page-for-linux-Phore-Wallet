d839a33d5ab2a0c675fcfc58f54a68a70ebbe7d6
Elio <Polielio@users.noreply.github.com>
2018-01-03 21:09:10 +0100

    Indentation

--- getphore.py
+++ getphore.py
@@ -14,17 +14,17 @@ i=0
 lastbalance = 0
 while i ==0:
     if time.time()-lasttime >=60:
-        lastime = time.time()
+    	lastime = time.time()
         p = subprocess.Popen(["phore-cli","getinfo"], stdout=subprocess.PIPE)
         output, err = p.communicate()
         o = json.loads(output)
         newbalance = o["balance"]
     	pa = '/var/www/html/phorec.json'
-		with open(pa, 'w') as outfile:
-			json.dump(o, outfile)
+	with open(pa, 'w') as outfile:
+		json.dump(o, outfile)
         if newbalance != lastbalance:
-            fo = open("/var/www/html/history.txt", "a+")
-            fo.write(strftime("%a, %d %b %Y %H:%M:%S", gmtime())+'  Balance: {0}'.format(newbalance)+'\n')
-            fo.close()
-            lastbalance = newbalance
+        	fo = open("/var/www/html/history.txt", "a+")
+           	fo.write(strftime("%a, %d %b %Y %H:%M:%S", gmtime())+'  Balance: {0}'.format(newbalance)+'\n')
+                fo.close()
+                lastbalance = newbalance
 		 
