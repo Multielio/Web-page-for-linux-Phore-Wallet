c18e9474c102bc55b86647f9601cd5b147a5b0ae
Elio <Polielio@users.noreply.github.com>
2018-01-05 18:04:37 +0100

    Many Changes

--- getphore.py
+++ getphore.py
@@ -8,7 +8,14 @@ Created on Wed Jan  3 18:11:39 2018
 import subprocess      
 import json
 import time
+from datetime import datetime, timedelta
 from time import gmtime, strftime
+
+
+def gtime(sec):
+    d = datetime(1,1,1) + timedelta(seconds=sec)
+    return "%d day(s), %d hour(s), %d minute(s), %d second(s)" % (d.day-1, d.hour, d.minute, d.second)
+
 web_path = "/var/www/html/p/{}"
 web_page="""
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -57,29 +64,39 @@ web_page="""
 			$file = "/home/python/history.txt";
 			echo file_get_contents($file);
 		?>	
-
 		<hr>
 
 
 
-		<i class="fa fa-flask fa-2x" aria-hidden="true"></i><h4>Phore Network</h4>
+       
+       <i class="fa fa-flask fa-2x" aria-hidden="true"></i><h4>Phore Network</h4>
 		<ul>
 		  <li> Difficulty : 
 		  {}
 		  </li>
-		  
-		</ul>
-		
-		<hr>
-		<i class="fa fa-bar-chart fa-2x" aria-hidden="true"></i><h4>zPHR Supply<h4>
-		<ul><li> Total:
-            {}
+         <li> Total zPHR Supply:
+		 {}
 			
 		</li>
 		</ul>
+		
+        
+		
+       
 		<hr>
 		<i class="fa fa-server fa-2x" aria-hidden="true"></i><h4> Masternode</h4>
+       
+       
+       <b> Some data : </b>
+       <ul>
+       <li>Online Masternode: {} </li>
+       <li>Time to next super block: {} </li>
+       </ul>
+       </br>
+       <b>Budget Proposal:</b>
+	   <div>
 		{}
+       
 		<hr>
 	
 		<h4>Usefull Links</h4>
@@ -93,6 +110,7 @@ web_page="""
 </body>
 </html>
 
+
 """
 
 #"""
@@ -104,6 +122,7 @@ web_page="""
 #"""
 
 mastpatern = """
+	     <div class="col-sm-6">
         <div class="panel panel-default">
         <div class="panel-heading"><b>Projet Name: {}</b></div>
         <div class="panel-body">
@@ -119,15 +138,12 @@ mastpatern = """
         <li>Ratio : {}</li>
         <li>TotalPayment : {}</li>
         <li>MonthlyPayment : {}</li>
-        <li>IsEstablished : {}</li>
-        <li>IsValid : {}</li>
-        <li>IsValidReason : {}</li>
-        <li>fValid : {}</li>
         <i class="fa fa-check fa-1x" aria-hidden="true"></i><a href="#/" class= "btn btn-success" > {} YES</a>
         <i class="fa fa-times fa-1x" aria-hidden="true"></i><a href="#/" class= "btn btn-danger" > {} NO</a>
-		  </ul>
-         </div>
-         </div>"""
+		 </ul>
+        </div>
+        </div>
+        </div>"""
 
 lasttime = time.time()
 i=0
@@ -135,6 +151,10 @@ lastbalance = 0
 while i ==0:
     if time.time()-lasttime >=60:
         lastime = time.time()
+        
+        #######################################################################################
+        # GET DATA 
+        #######################################################################################
         # Getting some info with phore-cli getinfo
         p = subprocess.Popen(["phore-cli","getinfo"], stdout=subprocess.PIPE)
         output, err = p.communicate()
@@ -144,19 +164,41 @@ while i ==0:
         p = subprocess.Popen(["phore-cli","getbudgetinfo"], stdout=subprocess.PIPE)
         output, err = p.communicate()
         oo = json.loads(output)
+        
+        #Getting info from phore-cli getbudgetinfo
+        p = subprocess.Popen(["phore-cli","getnextsuperblock"], stdout=subprocess.PIPE)
+        ooo, err = p.communicate()
+        
+        #Getting info from phore-cli getmasternodecount
+        p = subprocess.Popen(["phore-cli","getmasternodecount"], stdout=subprocess.PIPE)
+        output, err = p.communicate()
+        oooo = json.loads(output)
+        #######################################################################################
+        # Creating data to display
+        #######################################################################################
         masternodestring =""
+        l = 0
         for el in oo:
-            masternodestring +="</br>"+mastpatern.format(el["Name"],el["URL"],el["Hash"],el["FeeHash"],el["BlockStart"],el["BlockEnd"],el["TotalPaymentCount"],el["RemainingPaymentCount"],el["PaymentAddress"],el["Ratio"],el["TotalPayment"],el["MonthlyPayment"],el["IsEstablished"],el["IsValid"],el["IsValidReason"],el["fValid"],el["Yeas"],el["Nays"]) 
-        
-        #
-        
+            
+            if l%2 ==0:
+                if l != len(el):
+                    masternodestring +="""</div><div class="row">"""+mastpatern.format(el["Name"],el["URL"],el["Hash"],el["FeeHash"],el["BlockStart"],el["BlockEnd"],el["TotalPaymentCount"],el["RemainingPaymentCount"],el["PaymentAddress"],el["Ratio"],el["TotalPayment"],el["MonthlyPayment"],el["Yeas"],el["Nays"]) 
+                else:
+                    masternodestring +="""</div><div class="row">"""+mastpatern.format(el["Name"],el["URL"],el["Hash"],el["FeeHash"],el["BlockStart"],el["BlockEnd"],el["TotalPaymentCount"],el["RemainingPaymentCount"],el["PaymentAddress"],el["Ratio"],el["TotalPayment"],el["MonthlyPayment"],el["Yeas"],el["Nays"])+"</div>"
+            else:
+                masternodestring +=mastpatern.format(el["Name"],el["URL"],el["Hash"],el["FeeHash"],el["BlockStart"],el["BlockEnd"],el["TotalPaymentCount"],el["RemainingPaymentCount"],el["PaymentAddress"],el["Ratio"],el["TotalPayment"],el["MonthlyPayment"],el["Yeas"],el["Nays"]) +"</div>"
+            l+=1
         newbalance = o["balance"]
         fo = open(web_path.format("phore.php"), "w")
-        fo.write(web_page.format(o["balance"],o["staking status"],o["blocks"],o["difficulty"],o["zPHRsupply"]["total"],masternodestring))
+        fo.write(web_page.format(o["balance"],o["staking status"],o["blocks"],o["difficulty"],o["zPHRsupply"]["total"],oooo["total"],gtime(60*(int(ooo)-int(o["blocks"]))), masternodestring))
         fo.close()
-        
+        #######################################################################################
+        # STORE BALANCE HISTORY
+        #######################################################################################
         if newbalance != lastbalance:
             fo = open("history.txt", "a+")
             fo.write("\n"+strftime("%a, %d %b %Y %H:%M:%S", gmtime())+'  Balance: {0}'.format(newbalance))
             fo.close()
             lastbalance = newbalance
+            
+            
